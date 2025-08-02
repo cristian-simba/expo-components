f57498b2b0dcfd2737c67b668787a77e4098dff7
cristian-simba <davidsimba200@gmail.com>
2025-08-02 16:04:41 -0500

    use nativewind to update ligth and dark styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,12 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 import "./global.css"
- 
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
@@ -20,11 +21,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+      <View className='bg-light-background dark:bg-dark-background flex-1'>
+        <Text className='text-3xl mt-10 text-light-primary dark:text-dark-primary'>Hola mundo</Text>
+      </View>
     </ThemeProvider>
   );
 }
